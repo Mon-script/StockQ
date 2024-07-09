1bcba702f834678141eb7ce3c92d8d4ae2b787bb
Rodrigo <rodrigoamor94@gmail.com>
2024-07-09 20:03:07 -0300

    barrcod

--- cliente/src/pages/domain/Producto.jsx
+++ cliente/src/pages/domain/Producto.jsx
@@ -126,6 +126,18 @@ export const Producto = () => {
                   />
                 </div>
                 <div>
+                  <label htmlFor="codigo" className="block text-sm font-medium text-gray-700 mb-1">
+                    Intruducir codigo de barra:
+                  </label>
+                  <input
+                    id="codigo"
+                    className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                    type="number"
+                    required
+                    
+                  />
+                </div>
+                <div>
                   <label htmlFor="FOTO" className="block text-sm font-medium text-gray-700 mb-1">
                     Imagen:
                   </label>
