484735a9aed1b3604958131dcb4809e42b4567a9
Rodrigo Love <rodrigoamor94@gmail.com>
2024-07-12 07:36:03 -0300

    stockF

--- cliente/src/pages/Component/codigodebarra/CodigodeBarras2.jsx
+++ cliente/src/pages/Component/codigodebarra/CodigodeBarras2.jsx
@@ -1,27 +1,118 @@
 import React, { useState } from 'react';
 import "./CodigodeBarra2.css";
 
-const CodigodeBarras2 = ({Texto}) => {
+const CodigodeBarras2 = ({estadoForm ,Texto, idEntrada}) => {
   const [codigoBarras, setCodigoBarras] = useState('');
+  const [idEmpleado, setIdEmpleado] = useState('');
 
   const manejarCambioCodigoBarras = (event) => {
-    // Actualizar el estado con el código de barras ingresado
     setCodigoBarras(event.target.value);
   };
 
   const obtenerInformacionCodigoBarras = () => {
-    // Aquí puedes realizar acciones con el código de barras,
-    // como enviarlo a un servidor para obtener información.
-
-    // Por ahora, simplemente mostraremos la información en la consola.
-    console.log('Código de barras escaneado:', codigoBarras);
+    if(!codigoBarras || !idEntrada || !idEmpleado){
+      alert('Falta informacion para proceder')
+    }
+    //aqui va la peticion fetch para el DELETE de la tabla Entrada
+    if (window.confirm('¿Está seguro de enviar este producto a salida?')) {
+    fetch('url/' + idEntrada+'/'+ codigoBarras, {
+        method: 'DELETE'
+      })
+        .then(resp => resp.text())
+        .then(resp => {
+          alert(resp);
+         
+        });
+    } 
+    //aqui va la peticion fetch para el post de la tabla SALIDA
+    
+    console.log('Código de barras escaneado:', codigoBarras, 'empleado: ', idEmpleado);
+    estadoForm(false)
   };
 
   return (
     <div className='CodigoDeBarras2'>
       <label className= 'CodigoLabel2'htmlFor="codigoBarras">Ingrese el código de barras:</label>
       < input className='CodigoInput2'
-        type="text"
+        type="number"
+        id="codigoBarras"
+        value={codigoBarras}
+        onChange={manejarCambioCodigoBarras}
+      />
+      < input className='CodigoInput2'
+        type="number"
+        id="codigoBarras"
+        value={idEmpleado}
+        onChange={(e)=>{setIdEmpleado(e.target.value)}}
+      />
+      <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      <button className='CodigoButton2' onClick={obtenerInformacionCodigoBarras}>{Texto}</button>
+      <button className='CodigoButton2' onClick={estadoForm(false)}>{Texto}</button>
+      </div>
+    </div>
+  );
+};
+
+export default CodigodeBarras2;
+
+{/*modificr implementacion:
+  // TarjetaStock.jsx
+import React, { useState } from 'react';
+import CodigodeBarras2 from './CodigodeBarras2'; // Asegúrate de que la ruta sea correcta
+
+const TarjetaStock = ({ producto }) => {
+  const { nombre, peso, calidad, color } = producto;
+  const [mostrarFormularioSalida, setMostrarFormularioSalida] = useState(false);
+
+  const manejarSalida = () => {
+    setMostrarFormularioSalida(true);
+  };
+
+  return (
+    <div className="cards">
+      <div className="title-1">{nombre}</div>
+      <img src={Logo} alt="" className='w-[250px] h-[250px] border rounded p-6' />
+      <div className="content">
+        peso: {peso} <br />
+        calidad: {calidad} <br />
+        color: {color}
+      </div>
+      <button className="btn1" onClick={manejarSalida}>Salida</button>
+      <div className="bar">
+        <div className="emptybar"></div>
+        <div className="filledbar"></div>
+      </div>
+      <div className='CodigosdeB'>
+        {mostrarFormularioSalida && <CodigodeBarras2 estadoForm={setMostrarFormularioSalida} Texto="SALIDA" />}
+      </div>
+    </div>
+  );
+};
+
+export default TarjetaStock;
+
+// CodigodeBarras2.jsx
+import React, { useState } from 'react';
+
+const CodigodeBarras2 = ({ estadoForm, Texto }) => {
+  const [codigoBarras, setCodigoBarras] = useState('');
+
+  const manejarCambioCodigoBarras = (event) => {
+    setCodigoBarras(event.target.value);
+  };
+
+  const obtenerInformacionCodigoBarras = () => {
+    // Realiza acciones con el código de barras aquí
+    estadoForm(false); // Oculta el formulario
+    console.log('Código de barras escaneado:', codigoBarras);
+  };
+
+  return (
+    <div className='CodigoDeBarras2'>
+      <label className='CodigoLabel2' htmlFor="codigoBarras">Ingrese el código de barras:</label>
+      <input
+        className='CodigoInput2'
+        type="number"
         id="codigoBarras"
         value={codigoBarras}
         onChange={manejarCambioCodigoBarras}
@@ -32,3 +123,4 @@ const CodigodeBarras2 = ({Texto}) => {
 };
 
 export default CodigodeBarras2;
+ */ }
--- cliente/src/pages/domain/stock.jsx
+++ cliente/src/pages/domain/stock.jsx
@@ -2,8 +2,13 @@
 import TarjetaStock from "../Component/tarjetas/TarjetaStock";
 import Matriz from "../Component/matrizStock/matrizS";
 import "./Stockpag.css";
+import { useState, useEffect } from "react";
 
 export const Stock = () => {
+  const [showForm, setShowForm] = useState('')
+  const [codigoProducto, setCodigoProducto] = useState('')
+  const [estanteria, setEstanteria] = useState('')
+
   const numEstantes = 3;
   const numFilas = 2;
   const numColumnas = 3;
@@ -31,7 +36,111 @@ export const Stock = () => {
     // Otros productos...
   ];
 
-  return <Matriz estantes={matrizEjemplo} productos={productos} />;
+  const agregarEntrada = () => {
+    if (estanteria > numEstantes) {
+      alert('Debes indicar un estante existente')
+      return
+    }
+
+    const now = new Date(); // trae la fecha y hora actual
+    const fecha = now.toISOString().split('T')[0]; // Fecha en formato YYYY-MM-DD
+    const hora = now.toTimeString().split(' ')[0]; // Hora en formato HH:MM:SS
+
+    const data = {
+      codigo: codigoProducto,
+      estante: parseInt(estanteria),
+      fecha: fecha, 
+      hora: hora   
+    };
+
+    fetch('url', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(resp => resp.text())
+      .then(resp => alert(resp))
+      .catch(error => console.error('Error:', error));
+
+  }
+
+  return <>
+
+    <div className="min-h-screen flex flex-col items-center pt-4">
+      <div className="bg-gray-100 p-4 shadow-md w-full flex justify-between items-center">
+        <h2 className="text-3xl font-semibold text-red-700 text">Entrada de Stock!</h2>
+        <button
+          className="bg-red-500 text-white py-2 px-6 rounded-full hover:bg-red-600 transition duration-300"
+          onClick={() => {
+            setShowForm(true)
+            setCodigoProducto('');
+            setEstanteria('');
+
+          }}
+          hidden={showForm ? true : false}
+        >
+          Agregar
+        </button>
+      </div>
+      {showForm && (
+        <div className="flex flex-col items-center justify-center w-full mt-8">
+          <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-lg">
+            <form className="space-y-6 w-full">
+
+              <div>
+                <label htmlFor="codigoProducto" className="block text-sm font-medium text-gray-700 mb-1">
+                  Codigo de producto:
+                </label>
+                <input
+                  id="codigoProducto"
+                  className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                  type="number"
+                  required
+                  value={codigoProducto}
+                  onChange={(e) => setCodigoProducto(e.target.value)}
+
+                />
+              </div>
+              <div>
+                <label htmlFor="estanteria" className="block text-sm font-medium text-gray-700 mb-1">
+                  Estanteria:
+                </label>
+                <input
+                  id="estanteria"
+                  className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                  type="number"
+                  required
+                  value={estanteria}
+                  onChange={(e) => setEstanteria(e.target.value)}
+
+                />
+              </div>
+              <div className="flex justify-between mt-4">
+                <button
+                  className="bg-red-500 text-white py-2 px-4 rounded-full hover:bg-red-600 transition duration-300"
+                  type="button"
+                  onClick={agregarEntrada}
+
+                >
+                  Agregar
+                </button>
+                <button
+                  className="bg-gray-300 text-gray-700 py-2 px-4 rounded-full hover:bg-gray-400 transition duration-300"
+                  type="button"
+                  onClick={() => setShowForm(false)}
+                >
+                  Salir
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+
+    <Matriz estantes={matrizEjemplo} productos={productos} /> </>
 };
 
 /*export const Stock=()=>{
